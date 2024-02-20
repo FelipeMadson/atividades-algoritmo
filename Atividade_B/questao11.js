e51afc444648b338062f0daf13e2a8a8a3179366
Veloso <catce.2024111tads0012@aluno.ifpi.edu.br>
2024-02-20 00:16:51 -0300

    first commit

--- Atividade_B/questao11.js
+++ Atividade_B/questao11.js
@@ -1,14 +1,14 @@
 import { question } from 'readline-sync';
 
-// Solicitando ao usuário que insira o número inteiro de 3 dígitos
+// Entrada (inserir o número inteiro de 3 dígitos)
 var numero = question("Digite um número inteiro de 3 dígitos: ");
 
-// Verificando se o número tem 3 dígitos
+// Processamento (verificar se o número tem 3 dígitos)
 if (numero.length === 3) {
     // Convertendo o número para uma string, invertendo-a e, em seguida, convertendo-a de volta para um número inteiro
     var inverso = parseInt(numero.split('').reverse().join(''));
 
-    // Imprimindo o número inverso
+    // Entrada
     console.log("O inverso do número " + numero + " é: " + inverso);
 } else {
     console.log("O número inserido não tem 3 dígitos.");
